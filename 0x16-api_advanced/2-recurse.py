5eda3a2332c41e23d6820f343f793cf822b2d9e5
Azizo1212 <azizdali530@gmail.com>
2022-07-14 23:12:04 +0200

    2-main.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for task 2"""
+
+import requests
+import json
+
+def recurse(subreddit, list=[], count=0, after=None):
+
+
+    req = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "Aziz"},
+                            allow_redirects=False)
+
+    if req.status_code >= 400:
+        return None
+
+    ml = list + [child.get("data").get("title")
+                        for child in req.json().get("data")
+                        .get("children")]
+
+    reqs = req.json()
+    if not reqs.get("data").get("after"):
+        return ml
+
+    return recurse(subreddit, ml, reqs.get("data").get("count"),
+                   reqs.get("data").get("after"))
